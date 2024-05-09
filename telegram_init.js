40dfe4094d0f6993b09d80f3cb7a25116217a303
Yaroslav <yaroslav.cherkaev@yandex.ru>
2024-05-09 15:33:03 +0300

    new

--- telegram_init.js
+++ telegram_init.js
@@ -4,5 +4,6 @@ tg.expand();
 tg.MainButton.text = "Указать адресс"; //изменяем текст кнопки 
 tg.MainButton.textColor = "#F55353"; //изменяем цвет текста кнопки
 tg.MainButton.color = "#143F6B"; //изменяем цвет бэкграунда кнопки
+tg.MainButton.show();
 
 console.log("hello")
